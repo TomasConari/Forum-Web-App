5c11248f92df815f0658e35a9e23f84b6d48d670
Tomas Contreras Laptop <tomasconari@gmail.com>
2023-09-27 21:19:04 -0500

    Updated User Controller, Improved Error Management

--- Back/src/controllers/post.controllers.js
+++ Back/src/controllers/post.controllers.js
@@ -1,6 +1,4 @@
 import { Post } from "../models/post.model.js";
-import { User } from "../models/user.model.js";
-import { Comment } from "../models/comment.model.js";
 
 export const postControllers = {
     create: async (req, res) => {
