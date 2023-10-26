6785152f77f30c6057084c80d54affe232efa742
Tomas Contreras Laptop <tomasconari@gmail.com>
2023-10-25 22:46:51 -0500

    Added error handling for when there is no internet connection

--- Front/src/components/Dashboard.jsx
+++ Front/src/components/Dashboard.jsx
@@ -9,21 +9,20 @@ export const Dashboard = ({ host, header, localUser, window }) => {
     const [createPostDeploy, setCreatePostDeploy] = useState(false);
     const [allPostsOrder, setAllPostsOrder] = useState(false);
     const [allPosts, setallPosts] = useState([]);
-
-    let callError = "no Error";
+    const [callError, setCallError] = useState("");
 
     const allPostsCall = async () => {
         try{
             const rawData = await fetch(`${host}/posts`, {
-            method: "GET",
-            headers: header
+                method: "GET",
+                headers: header
             });
             const dataJson = await rawData.json();
             const dataArr = dataJson.data;
             setallPosts(dataArr.reverse());
         }catch(error){
             console.error("Error fetching Posts:", error);
-            callError = "No Connection";
+            setCallError("No Connection")
         };
     };
 
@@ -37,16 +36,72 @@ export const Dashboard = ({ host, header, localUser, window }) => {
         };
     };
 
+    console.log(callError)
+
     useEffect(() => {
         allPostsCall();
     }, []);
 
-    if(allPosts.length > 0){
+    if(callError === "No Connection"){
+        return(
+            <>
+                <div style={dashboardStyles.header}><h1 style={ window.width > 200 ? dashboardStyles.title : dashboardStyles.hiddenTitle }>Forum</h1></div>
+                <div style={dashboardStyles.dashboard}>
+                    <h1>Connection Lost, Try: <br />
+                        <h5>
+                            -Check Your Internet Connection.<br />
+                            -Reload The Page.<br />
+                            -Log in Again.<br />
+                            -Try Again Later.
+                        </h5>
+                    </h1>
+                </div>
+            </>
+        );
+    }else if(allPosts.length > 0){
         return(
             <>
                 <div style={dashboardStyles.header}><h1 style={ window.width > 200 ? dashboardStyles.title : dashboardStyles.hiddenTitle }>Forum</h1></div>
                     <div style={dashboardStyles.dashboard}>
-                    <button onClick={() => setCreatePostDeploy(!createPostDeploy)}>New Post</button>
+                        <button onClick={() => setCreatePostDeploy(!createPostDeploy)}>New Post</button>
+                        <button onClick={invertPostsOrder}>{allPostsOrder? "Showing Oldest Posts First" : "Showing Newest Posts First"}</button>
+                        {createPostDeploy && 
+                            <FormToCreate 
+                                createQuote="Post"
+                                hostProp={host}
+                                localUsername={localUser.username}
+                                headerProp={header}
+                                reCall={allPostsCall}
+                                setMessageProp={setPostResponse}
+                                setDeploy={setCreatePostDeploy}
+                            />
+                        }
+                        {postResponse && <p>{postResponse}</p>}
+                        <br />
+                        <div>
+                            {allPosts.map((el) => (
+                                <Post 
+                                    hostProp={host} 
+                                    headerProp={header} 
+                                    localUser={localUser} 
+                                    user={el.user} 
+                                    title={el.title} 
+                                    text={el.text} 
+                                    id={el["_id"]} 
+                                    key={el["_id"]} 
+                                    postReCall={allPostsCall}
+                                />
+                            ))}
+                    </div>
+                </div>
+            </>
+        );
+    }else{
+        return(
+            <>
+                <div style={dashboardStyles.header} ><h1 style={ window.width > 200 ? dashboardStyles.title : dashboardStyles.hiddenTitle }>Forum</h1></div>
+                <div style={dashboardStyles.dashboard} >
+                    {!createPostDeploy && <button onClick={() => setCreatePostDeploy(!createPostDeploy)}>New Post</button>}
                     <button onClick={invertPostsOrder}>{allPostsOrder? "Showing Oldest Posts First" : "Showing Newest Posts First"}</button>
                     {createPostDeploy && 
                         <FormToCreate 
@@ -60,49 +115,8 @@ export const Dashboard = ({ host, header, localUser, window }) => {
                         />
                     }
                     {postResponse && <p>{postResponse}</p>}
-                    <br />
-                    <div>
-                        {allPosts.map((el) => (
-                            <Post 
-                                hostProp={host} 
-                                headerProp={header} 
-                                localUser={localUser} 
-                                user={el.user} 
-                                title={el.title} 
-                                text={el.text} 
-                                id={el["_id"]} 
-                                key={el["_id"]} 
-                                postReCall={allPostsCall}
-                            />
-                        ))}
-                    </div>
                 </div>
             </>
-        );
-    }else if(callError = "No Connection"){
-        return(
-            <div>
-                <h1>No Internet Connection, Try Again Later</h1>
-            </div>
-        );
-    }else{
-        return(
-            <div>
-                {!createPostDeploy && <button onClick={() => setCreatePostDeploy(!createPostDeploy)}>New Post</button>}
-                <button onClick={invertPostsOrder}>{allPostsOrder? "Showing Oldest Posts First" : "Showing Newest Posts First"}</button>
-                {createPostDeploy && 
-                    <FormToCreate 
-                        createQuote="Post"
-                        hostProp={host}
-                        localUsername={localUser.username}
-                        headerProp={header}
-                        reCall={allPostsCall}
-                        setMessageProp={setPostResponse}
-                        setDeploy={setCreatePostDeploy}
-                    />
-                }
-                {postResponse && <p>{postResponse}</p>}
-            </div>
         );
     };
 };
